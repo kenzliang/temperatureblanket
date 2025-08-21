68fbb673a4900ee2ff9d62e546b9b547db6d1a72
Zhi Liang <me@kenzliang.com>
2025-08-20 20:44:29 -0400

    fix viewability dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ export default function App() {
     <div className="max-w-5xl mx-auto p-4 space-y-6">
       <header className="flex items-center justify-between">
         <h1 className="text-3xl font-extrabold text-gray-800 dark:text-white">
-          Weather Checks
+          Valerie's Temperature Blanket Project
         </h1>
         <div className="flex items-center gap-2">
           <label className="label">Date</label>
